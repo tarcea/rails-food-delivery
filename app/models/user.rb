228214af636f4219125c11be983d1d8db6b10c9f
Gheorghe Tarcea <tarcea@gmail.com>
2020-05-01 17:41:31 +0200

    manager vs delivery boy trials

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  def manager
+    current_user.role == 'manager'
+  end
 end
